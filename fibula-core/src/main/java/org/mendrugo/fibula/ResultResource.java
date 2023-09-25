9bd72267dc02a17393f7488d61692588e5d42b4a
Galder Zamarreño <galder@zamarreno.com>
2023-09-25 23:28:54 +0200

    Print out throughput stats

--- fibula-core/src/main/java/org/mendrugo/fibula/ResultResource.java
+++ fibula-core/src/main/java/org/mendrugo/fibula/ResultResource.java
@@ -10,7 +10,50 @@ public class ResultResource
     @POST
     public String add(ThroughputResult result)
     {
-        System.out.println("Received: " + result);
+        final int COLUMN_PAD = 2;
+
+        // todo unfix
+        final String benchmarkName = "FibulaSample_01_HelloWorld.helloWorld";
+        final String modeName = "thrpt";
+
+        int nameLength = "Benchmark".length();
+        nameLength = Math.max(nameLength, benchmarkName.length());
+
+        int modeLength = "Mode".length();
+        modeLength = Math.max(modeLength, modeName.length());
+
+        int samplesLength = "Cnt".length();
+
+        int scoreLength = "Score".length();
+        scoreLength = Math.max(scoreLength, ScoreFormatter.format(result.statistic()).length());
+
+        int scoreErrLength = "Error".length();
+
+        int unitLength = "Units".length();
+
+        modeLength += COLUMN_PAD;
+        samplesLength += COLUMN_PAD;
+        scoreLength += COLUMN_PAD;
+        scoreErrLength += COLUMN_PAD - 1; // digest a single character for +- separator
+        unitLength += COLUMN_PAD;
+
+        System.out.printf("%-" + nameLength + "s", "Benchmark");
+        System.out.printf("%" + modeLength + "s", "Mode");
+        System.out.printf("%" + samplesLength + "s", "Cnt");
+        System.out.printf("%" + scoreLength + "s", "Score");
+        System.out.print("  ");
+        System.out.printf("%" + scoreErrLength + "s", "Error");
+        System.out.printf("%" + unitLength + "s",     "Units");
+        System.out.println();
+
+        System.out.printf("%-" + nameLength + "s", benchmarkName);
+        System.out.printf("%" + modeLength + "s", modeName);
+        System.out.print(ScoreFormatter.format(scoreLength, result.statistic()));
+        System.out.print("  ");
+        System.out.printf("%" + scoreErrLength + "s", "");
+        System.out.printf("%" + unitLength + "s", result.unit());
+        System.out.println();
+
         return "OK";
     }
 }
---
+++ fibula-core/src/main/java/org/mendrugo/fibula/ScoreFormatter.java
@@ -0,0 +1,55 @@
+package org.mendrugo.fibula;
+
+public class ScoreFormatter
+{
+    private static final int PRECISION = Integer.getInteger("jmh.scorePrecision", 3);
+    private static final double ULP = 1.0 / Math.pow(10, PRECISION);
+    private static final double THRESHOLD = ULP / 2;
+
+    public static boolean isApproximate(double score)
+    {
+        return (score < THRESHOLD);
+    }
+
+    public static String format(double score)
+    {
+        if (isApproximate(score))
+        {
+            int power = (int) Math.round(Math.log10(score));
+            return "\u2248 " + ((power != 0) ? "10" + superscript("" + power) : "0");
+        }
+        else
+        {
+            return String.format("%." + PRECISION + "f", score);
+        }
+    }
+
+    public static String format(int width, double score)
+    {
+        if (isApproximate(score))
+        {
+            int power = (int) Math.round(Math.log10(score));
+            return String.format("%" + width + "s", "\u2248 " + ((power != 0) ? "10" + superscript("" + power) : "0"));
+        }
+        else
+        {
+            return String.format("%" + width + "." + PRECISION + "f", score);
+        }
+    }
+
+    public static String superscript(String str)
+    {
+        str = str.replaceAll("-", "\u207b");
+        str = str.replaceAll("0", "\u2070");
+        str = str.replaceAll("1", "\u00b9");
+        str = str.replaceAll("2", "\u00b2");
+        str = str.replaceAll("3", "\u00b3");
+        str = str.replaceAll("4", "\u2074");
+        str = str.replaceAll("5", "\u2075");
+        str = str.replaceAll("6", "\u2076");
+        str = str.replaceAll("7", "\u2077");
+        str = str.replaceAll("8", "\u2078");
+        str = str.replaceAll("9", "\u2079");
+        return str;
+    }
+}
