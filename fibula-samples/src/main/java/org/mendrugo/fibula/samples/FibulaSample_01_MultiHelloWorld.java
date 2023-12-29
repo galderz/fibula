087cf1e1748bf546085698654139b1b4be14cb9d
Galder Zamarreño <galder@zamarreno.com>
2023-12-29 11:47:52 +0000

    Add multi method example

---
+++ fibula-samples/src/main/java/org/mendrugo/fibula/samples/FibulaSample_01_MultiHelloWorld.java
@@ -0,0 +1,16 @@
+package org.mendrugo.fibula.samples;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class FibulaSample_01_MultiHelloWorld
+{
+    @Benchmark
+    public void helloWorld1()
+    {
+    }
+
+    @Benchmark
+    public void helloWorld2()
+    {
+    }
+}
