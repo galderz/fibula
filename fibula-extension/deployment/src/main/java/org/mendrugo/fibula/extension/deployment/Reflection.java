5f97b6ac1667b2434ac007bae1c84ad72cfde4cb
Galder Zamarreño <galder@zamarreno.com>
2024-04-15 19:03:01 +0200

    Remove unused code

--- fibula-extension/deployment/src/main/java/org/mendrugo/fibula/extension/deployment/Reflection.java
+++ fibula-extension/deployment/src/main/java/org/mendrugo/fibula/extension/deployment/Reflection.java
@@ -1,7 +1,6 @@
 package org.mendrugo.fibula.extension.deployment;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public final class Reflection
