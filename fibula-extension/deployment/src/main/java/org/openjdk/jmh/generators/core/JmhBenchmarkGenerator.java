4bed9b6602bf34685adfb1f90be722e55d8ef7d4
Galder Zamarreño <galder@zamarreno.com>
2024-05-02 16:36:02 +0200

    Add perf norm test

--- fibula-extension/deployment/src/main/java/org/openjdk/jmh/generators/core/JmhBenchmarkGenerator.java
+++ fibula-extension/deployment/src/main/java/org/openjdk/jmh/generators/core/JmhBenchmarkGenerator.java
@@ -173,6 +173,7 @@ public final class JmhBenchmarkGenerator extends BenchmarkGenerator
         final boolean supported = fqn.startsWith("org.mendrugo.fibula.it")
             || fqn.startsWith("org.openjdk.jmh.it.interorder.BenchmarkStateOrderTest")
             || fqn.startsWith("org.openjdk.jmh.it.profilers.LinuxPerfProfiler")
+            || fqn.startsWith("org.openjdk.jmh.it.profilers.LinuxPerfNormProfilerTest")
             || fqn.contains("JMHSample_01")
             || fqn.contains("JMHSample_03")
             || fqn.contains("JMHSample_04")
---
+++ fibula-it/src/test/java/org/mendrugo/fibula/it/profilers/LinuxPerfNormProfilerTest.java
@@ -0,0 +1,67 @@
+package org.mendrugo.fibula.it.profilers;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mendrugo.fibula.bootstrap.BenchmarkService;
+import org.openjdk.jmh.it.Fixtures;
+import org.openjdk.jmh.it.profilers.ProfilerTestUtils;
+import org.openjdk.jmh.profile.LinuxPerfNormProfiler;
+import org.openjdk.jmh.profile.ProfilerException;
+import org.openjdk.jmh.results.Result;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Map;
+
+@QuarkusTest
+public class LinuxPerfNormProfilerTest extends org.openjdk.jmh.it.profilers.LinuxPerfNormProfilerTest
+{
+    @Inject
+    BenchmarkService benchmarkService;
+
+    @Test
+    public void test() throws RunnerException
+    {
+        try
+        {
+            new LinuxPerfNormProfiler("");
+        }
+        catch (ProfilerException e)
+        {
+            System.out.println("Profiler is not supported or cannot be enabled, skipping test");
+            return;
+        }
+
+        Options opts = new OptionsBuilder()
+            .include(Fixtures.getTestMask(this.getClass().getSuperclass()))
+            .addProfiler(LinuxPerfNormProfiler.class)
+            .build();
+
+        RunResult rr = benchmarkService.runSingle(opts);
+
+        Map<String, Result> sr = rr.getSecondaryResults();
+        double instructions = ProfilerTestUtils.checkedGet(sr, "instructions", "instructions:u").getScore();
+        double cycles = ProfilerTestUtils.checkedGet(sr, "cycles", "cycles:u").getScore();
+        double branches = ProfilerTestUtils.checkedGet(sr, "branches", "branches:u").getScore();
+
+        Assert.assertNotEquals(0D, instructions, 0D);
+        Assert.assertNotEquals(0D, cycles, 0D);
+        Assert.assertNotEquals(0D, branches, 0D);
+
+        if (branches > instructions)
+        {
+            throw new IllegalStateException(String.format("Branches (%.2f) larger than instructions (%.3f)", branches, instructions));
+        }
+
+        double ipc = ProfilerTestUtils.checkedGet(sr, "IPC").getScore();
+        double cpi = ProfilerTestUtils.checkedGet(sr, "CPI").getScore();
+
+        Assert.assertNotEquals(0D, ipc, 0D);
+        Assert.assertNotEquals(0D, cpi, 0D);
+    }
+}
