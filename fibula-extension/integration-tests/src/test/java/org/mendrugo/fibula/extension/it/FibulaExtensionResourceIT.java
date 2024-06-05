9c59fe4f424d59afb7586939441f2bd75acdda28
Galder Zamarreño <galder@zamarreno.com>
2024-06-05 14:21:55 +0200

    Remove unnecessary readme files and maven plugins

--- fibula-extension/integration-tests/src/test/java/org/mendrugo/fibula/extension/it/FibulaExtensionResourceIT.java
+++
@@ -1,7 +0,0 @@
-package org.mendrugo.fibula.extension.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class FibulaExtensionResourceIT extends FibulaExtensionResourceTest {
-}
