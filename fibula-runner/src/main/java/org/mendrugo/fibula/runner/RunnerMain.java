b7729da23f27a56b3721a3d938e5f86abc7e5797
Galder Zamarreño <galder@zamarreno.com>
2024-10-29 17:02:22 +0100

    RunnerMain only used for native so no need to check if it's native

--- fibula-runner/src/main/java/org/mendrugo/fibula/runner/RunnerMain.java
+++ fibula-runner/src/main/java/org/mendrugo/fibula/runner/RunnerMain.java
@@ -17,32 +17,20 @@ public class RunnerMain implements QuarkusApplication
             final Method mainMethod = clazz.getDeclaredMethod("main", String[].class);
             mainMethod.setAccessible(true);
 
-            String[] invokeArgs;
-            // TODO this check might not be needed because this class only runs when native is enabled,
-            //      otherwise ForkedMain is run directly
-            if (isNativeRun())
-            {
-                // If more than 2 arguments passed in, keep only the last two
-                invokeArgs = new String[2];
-                invokeArgs[0] = args[args.length - 2];
-                invokeArgs[1] = args[args.length - 1];
-            }
-            else
-            {
-                invokeArgs = args;
-            }
-            
+            // The arguments are built by JMH and it assumes it invokes a Java application,
+            // but this runner only gets invoked for native executions,
+            // so only the last 2 arguments are relevant,
+            // the rest can be ignored.
+            final String[] invokeArgs = new String[2];
+            invokeArgs[0] = args[args.length - 2];
+            invokeArgs[1] = args[args.length - 1];
+
             mainMethod.invoke(null, new Object[]{invokeArgs});
         }
         catch (Exception e)
         {
             throw new RuntimeException(e);
         }
-    }
-
-    private static boolean isNativeRun()
-    {
-        return "runtime".equals(System.getProperty("org.graalvm.nativeimage.imagecode"));
     }
 
     @Override
