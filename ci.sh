e1f271f2dc84f525bba3d5407c6da7adc1612fc8
Galder Zamarreño <galder@zamarreno.com>
2024-11-22 17:06:29 +0100

    Adjust local ci script to simplification refactoring

--- ci.sh
+++ ci.sh
@@ -4,14 +4,11 @@ set -e -x
 
 test()
 {
-    local test_target=$1
-    local run_target=$2
+    local run_target=$1
 
-    make clean
-    make $test_target
     BENCHMARK=JMHSample_01 make $run_target
     BENCHMARK=FibulaSample_01_MultiHelloWorld make $run_target
 }
 
-test "test" "run"
-test "test-native" "run-native"
+make clean
+test "run-native"
