12936beea49a1bcb5b835757897cd135993525cc
Galder Zamarreño <galder@zamarreno.com>
2024-12-05 15:17:20 +0100

    Run blackhole JMH sample on the local ci script

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ sample()
 {
     local run_target=$1
 
-    BENCHMARK=JMHSample_01 make $run_target
+    BENCHMARK=JMHSample_09 make $run_target
     BENCHMARK=FibulaSample_01_MultiHelloWorld make $run_target
 }
 
