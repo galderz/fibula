11bb06a3e30cfae61fb6d9319ee9cac1bf16d2d3
Galder Zamarreño <galder@zamarreno.com>
2025-06-02 18:42:34 +0200

    Reuse refactored JMH Main class

--- fibula-core/src/main/java/org/mendrugo/fibula/MutiVmMain.java
+++ fibula-core/src/main/java/org/mendrugo/fibula/MutiVmMain.java
@@ -1,106 +1,26 @@
 package org.mendrugo.fibula;
 
-import org.openjdk.jmh.runner.Defaults;
-import org.openjdk.jmh.runner.NoBenchmarksException;
-import org.openjdk.jmh.runner.ProfilersFailedException;
+import org.openjdk.jmh.Main;
 import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.CommandLineOptionException;
 import org.openjdk.jmh.runner.options.CommandLineOptions;
-import org.openjdk.jmh.runner.options.VerboseMode;
 
 import java.io.IOException;
 
-public class MutiVmMain
+public class MutiVmMain extends Main
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] initialArgs) throws IOException
     {
-        try
-        {
-            final Pgo pgo = Pgo.instance();
-            args = pgo.preProcessArgs(args);
+        final MutiVmMain main = new MutiVmMain();
 
-            final CommandLineOptions cmdOptions = new CommandLineOptions(args);
+        final Pgo pgo = Pgo.instance();
+        final String[] args = pgo.preProcessArgs(initialArgs);
 
-            final Runner runner = pgo.isEnabled()
-                ? new MultiVmPgoRunner(cmdOptions)
-                : new MultiVmRunner(cmdOptions);
+        final CommandLineOptions cmdOptions = main.createCommandLineOptions(args);
 
-            if (cmdOptions.shouldHelp())
-            {
-                cmdOptions.showHelp();
-                return;
-            }
+        final Runner runner = pgo.isEnabled()
+            ? new MultiVmPgoRunner(cmdOptions)
+            : new MultiVmRunner(cmdOptions);
 
-            if (cmdOptions.shouldList())
-            {
-                runner.list();
-                return;
-            }
-
-            if (cmdOptions.shouldListWithParams())
-            {
-                runner.listWithParams(cmdOptions);
-                return;
-            }
-
-            if (cmdOptions.shouldListProfilers())
-            {
-                cmdOptions.listProfilers();
-                return;
-            }
-
-            if (cmdOptions.shouldListResultFormats())
-            {
-                cmdOptions.listResultFormats();
-                return;
-            }
-
-            try
-            {
-                runner.run();
-            }
-            catch (NoBenchmarksException e)
-            {
-                System.err.println("No matching benchmarks. Miss-spelled regexp?");
-
-                if (cmdOptions.verbosity().orElse(Defaults.VERBOSITY) != VerboseMode.EXTRA)
-                {
-                    System.err.println("Use " + VerboseMode.EXTRA + " verbose mode to debug the pattern matching.");
-                }
-                else
-                {
-                    runner.list();
-                }
-                System.exit(1);
-            }
-            catch (ProfilersFailedException e)
-            {
-                Throwable ex = e;
-                while (ex != null)
-                {
-                    System.err.println(ex.getMessage());
-                    for (Throwable supp : ex.getSuppressed())
-                    {
-                        System.err.println(supp.getMessage());
-                    }
-                    ex = ex.getCause();
-                }
-                System.exit(1);
-            }
-            catch (RunnerException e)
-            {
-                System.err.print("ERROR: ");
-                e.printStackTrace(System.err);
-                System.exit(1);
-            }
-
-        }
-        catch (CommandLineOptionException e)
-        {
-            System.err.println("Error parsing command line:");
-            System.err.println(" " + e.getMessage());
-            System.exit(1);
-        }
+        main.run(cmdOptions, runner);
     }
 }
